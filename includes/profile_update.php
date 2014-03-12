8053002601c4c5d2747fe84c69f23cf24b0baddd
Davie <funkstar33@users.noreply.github.com>
2014-03-12 10:22:44 +0000

    Update profile_update.php
    Updated to work with members profile.

--- includes/profile_update.php
+++ includes/profile_update.php
@@ -6,30 +6,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$website = yasDB_clean($_POST['website']);
 	$name = yasDB_clean($_POST['name']);
 	$email = yasDB_clean($_POST['email']);
-	$location = yasDB_clean($_POST['location']);
+  $location = yasDB_clean($_POST['location']);
 	$job = yasDB_clean($_POST['job']);
 	$aboutme = yasDB_clean($_POST['aboutme']);
-	$hobbies = yasDB_clean($_POST['hobbies']);
-	$gender_radio = yasDB_clean($_POST['radio']);
-	$month = yasDB_clean($_POST['DateOfBirth_Month']);
-	$day = yasDB_clean($_POST['DateOfBirth_Day']);
-	$year = yasDB_clean($_POST['DateOfBirth_Year']);
-	if ($month == "-Month-" || $day == "-Day-" || $year == "-Year-") {
-		$birthday = 0;
-	} else {
-		$birthday = strtotime($day." ".$month." ".$year);
-	}
-	if ($gender_radio == 'female') {
-		$gender = 'female';
-	}
-	elseif ($gender_radio == 'male') {
-		$gender = 'male';
-	}
-	else {
-		$gender = '';
-	}
+	$aim = yasDB_clean($_POST['aim']);
+	$msn = yasDB_clean($_POST['msn']);
+	$skype = yasDB_clean($_POST['skype']);
+	$yahoo = yasDB_clean($_POST['yahoo']);
+	$cmtsdisabled = yasDB_clean($_POST['cmtsdisabled']);
 	if (isset($_SESSION['userid'])) {
-		yasDB_update("UPDATE `user` SET website = '$website', name = '$name', email = '$email', location='$location', job='$job', aboutme='$aboutme', hobbies='$hobbies', birthday='$birthday', gender='$gender'  WHERE id = '$userid'");
+		yasDB_update("UPDATE `user` SET website = '$website', name = '$name', email = '$email', location='$location', job='$job', aboutme='$aboutme', aim='$aim', msn='$msn', skype='$skype', yahoo='$yahoo', cmtsdisabled='$cmtsdisabled' WHERE id = '$userid'");
 		echo '<h2>Your profile has been updated.</h2>';
 	}
 	else {
