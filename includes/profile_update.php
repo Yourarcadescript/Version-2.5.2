206c76ea2ecd0c4d5a80c3ce7e539b92a91b9731
Davie <funkstar33@users.noreply.github.com>
2014-04-01 08:12:10 +0100

    Update profile_update.php

--- includes/profile_update.php
+++ includes/profile_update.php
@@ -9,13 +9,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$location = yasDB_clean($_POST['location']);
 	$job = yasDB_clean($_POST['job']);
 	$aboutme = yasDB_clean($_POST['aboutme']);
-	$aim = yasDB_clean($_POST['aim']);
-	$msn = yasDB_clean($_POST['msn']);
-	$skype = yasDB_clean($_POST['skype']);
-	$yahoo = yasDB_clean($_POST['yahoo']);
+	$hobbies = yasDB_clean($_POST['hobbies']);
+	$shhobs = yasDB_clean($_POST['shhobs']);
+	$shloc = yasDB_clean($_POST['shloc']);
+	$sheml = yasDB_clean($_POST['sheml']);
+	$shname = yasDB_clean($_POST['shname']);
+	$shabout = yasDB_clean($_POST['shabout']);
+	$deact = yasDB_clean($_POST['deact']);
 	$cmtsdisabled = yasDB_clean($_POST['cmtsdisabled']);
 	if (isset($_SESSION['userid'])) {
-		yasDB_update("UPDATE `user` SET website = '$website', name = '$name', email = '$email', location='$location', job='$job', aboutme='$aboutme', aim='$aim', msn='$msn', skype='$skype', yahoo='$yahoo', cmtsdisabled='$cmtsdisabled' WHERE id = '$userid'");
+		yasDB_update("UPDATE `user` SET website = '$website', name = '$name', email = '$email', location='$location', job='$job', aboutme='$aboutme', hobbies='$hobbies', shhobs='$shhobs', cmtsdisabled='$cmtsdisabled', shloc='$shloc', sheml='$sheml', shname='$shname', shabout='$shabout', deact='$deact' WHERE id = '$userid'");
 		echo '<h2>Your profile has been updated.</h2>';
 	}
 	else {
