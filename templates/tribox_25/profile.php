ae38935c0b59273e3a1bba353e3245158eb971c5
Davie <funkstar33@users.noreply.github.com>
2014-04-01 08:08:46 +0100

    Update profile.php

--- templates/tribox_25/profile.php
+++ templates/tribox_25/profile.php
@@ -13,49 +13,21 @@ if(isset($_SESSION["user"])) {
   include("footer.php");
 exit;
 }
-if(isset($_GET['delete'])) {
-  $query = yasDB_select("SELECT id FROM user WHERE id = '{$_GET['delete']}'",false);
-  if ($query->num_rows == 0) {
-  if ($setting['seo'] == 'yes') {
-  $profile = $setting['siteurl'].'profile.html';
-  } else if($setting['seo'] == 'no'){
-  $profile = $setting['siteurl'].'index.php?act=profile';
-  }
-  ?>
-  <div class="edit_profile">
-  <center>You cannot delete an account that has been deleted.<br />
-  <a href="<?php echo $profile;?>">Click here to go back</a></center>
-  <div class="clear"></div></div></div>
-  <?php include ("footer.php");?>
-<?php
-  exit;
-  } else {
-  yasDB_delete("DELETE FROM user WHERE id = '{$_GET['delete']}'",false);
-  session_unset();
-  session_destroy();
-  ?>
-  <div class="edit_profile">
-  <center>Your account is now deleted.<br />Come back any time.<br />
-  <a href="<?php echo $setting['siteurl'].'index.php';?>">Click here to proceed</a></center>
-  <div class="clear"></div></div></div>
-  <?php include("footer.php");?>
-<?php
-  exit;
-  }
-}
 if(isset($_POST['settings'])) {
 	$website = yasDB_clean($_POST['website']);
 	$name = yasDB_clean($_POST['name']);
-	$email = yasDB_clean($_POST['email'])
-	;$location = yasDB_clean($_POST['location']);
+	$email = yasDB_clean($_POST['email']);
+  $location = yasDB_clean($_POST['location']);
 	$job = yasDB_clean($_POST['job']);
 	$aboutme = yasDB_clean($_POST['aboutme']);
-	$aim = yasDB_clean($_POST['aim']);
-	$msn = yasDB_clean($_POST['msn']);
-	$skype = yasDB_clean($_POST['skype']);
-	$yahoo = yasDB_clean($_POST['yahoo']);
-	$cmtsdisabled = yasDB_clean($_POST['cmtsdisabled']);
-	yasDB_update("UPDATE user SET website = '$website', name = '$name', email = '$email', location='$location', job='$job', aboutme='$aboutme', aim='$aim', msn='$msn', skype='$skype', yahoo='$yahoo', cmtsdisabled='$cmtsdisabled' WHERE username = '$user'");
+  $hobbies = yasDB_clean($_POST['hobbies']);
+  $shhobs = yasDB_clean($_POST['shhobs']);
+  $shloc = yasDB_clean($_POST['shloc']);
+  $sheml = yasDB_clean($_POST['sheml']);
+  $shname = yasDB_clean($_POST['shname']);
+  $deact = yasDB_clean($_POST['deact']);
+  $cmtsdisabled = yasDB_clean($_POST['cmtsdisabled']);
+	yasDB_update("UPDATE user SET website = '$website', name = '$name', email = '$email', location='$location', job='$job', aboutme='$aboutme', hobbies='$hobbies', shhobs='$shhobs', cmtsdisabled='$cmtsdisabled', shloc='$shloc', sheml='$sheml', shname='$shname', deact='$deact' WHERE username = '$user'");
 	if(!empty($_POST['password'])) {
 	$password = md5(yasDB_clean($_POST['password']));
 	yasDB_update("UPDATE user SET password = '$password' WHERE username = '$user'");
@@ -64,32 +36,123 @@ if(isset($_POST['settings'])) {
 } else {
 	$query = yasDB_select("SELECT * FROM `user` WHERE username = '$user'");
 	$row = $query->fetch_array(MYSQLI_ASSOC);
-	echo ' <div id="edit_profile"><div id="preview"></div><div id="profileBox">'.$user.'s Profile:
+	echo ' <div id="edit_profile"><div id="preview"></div><div id="profileBox">
 	<form name="usersettings" id="profile" method="post" action="index.php?act=profile">
-	Name:<br /><input type="text" name="name" value="' . $row['name'] . '" size="50" /><p>
-	Email:<br /><input type="text" name="email" value="' . $row['email'] . '" size="50" /><p>
-	Website:(Remember the http://)<br /><input type="text" name="website" value="' . $row['website'] . '" size="50" /><p>
-	Location:<br /><input type="text" name="location" value="' . $row['location'] . '" size="50" /><p>
-	Occupation:<br /><input type="text" name="job" value="' . $row['job'] . '" size="50" /><p>
-	About Me:<br /><textarea name="aboutme" rows="5" cols="38">'. $row['aboutme'] . '</textarea><p>
-	AIM:<br /><input type="text" name="aim" value="' . $row['aim'] . '" size="50" /><p>
-	MSN:<br /><input type="text" name="msn" value="' . $row['msn'] . '" size="50" /><p>
-	Skype:<br /><input type="text" name="skype" value="' . $row['skype'] . '" size="50" /><p>
-	Yahoo:<br /><input type="text" name="yahoo" value="' . $row['yahoo'] . '" size="50" /><p>
-	Disable Profile Comments:<br />
-	<select name="cmtsdisabled">
-	<option value="'.$row['cmtsdisabled'].'"/>'.$row['cmtsdisabled'].'</option>
-	<option value="yes">yes</option>
-	<option value="no">no</option>
-	</select><p>';
+<table class="tg">
+  <tr>
+    <th class="tg-s6z2" colspan="4">'.$user.'s Profile:</th>
+  </tr>
+  <tr>
+    <td class="tg-vn4c">Name:</td>
+    <td class="tg-vn4c"><input type="text" name="name" value="' . $row['name'] . '" size="24" /></td>
+    <td class="tg-vn4c">Email:</td>
+    <td class="tg-vn4c"><input type="text" name="email" value="' . $row['email'] . '" size="24" /></td>
+  </tr>
+  <tr>
+    <td class="tg-0ord">Website:(Remember the http://)</td>
+    <td class="tg-0ord"><input type="text" name="website" value="' . $row['website'] . '" size="24" /></td>
+    <td class="tg-Oord">Location:</td>
+    <td class="td-Oord"><input type="text" name="location" value="' . $row['location'] . '" size="24" /></td>
+  </tr>
+  <tr>
+    <th class="tg-s6z2" colspan="4">About Me:</th>
+  </tr>
+  <tr>
+    <td class="tg-vn4c" colspan="1">Interests:</td>
+    <td class="tg-vn4c" colspan="3"><textarea name="aboutme" rows="8" cols="50">'. $row['aboutme'] . '</textarea></td>
+  </tr>
+  <tr>
+    <td class="tg-Oord" colspan="1">Hobbies:</td>
+    <td class="tg-Oord" colspan="3"><textarea name="hobbies" rows="8" cols="50">'. $row['hobbies'] . '</textarea></td>
+  </tr>  
+  <tr>
+    <th class="tg-s6z2" colspan="4">Settings:</th>
+  </tr>
+  <tr>
+    <td class="tg-vn4c">Profile Comments:</td>
+    <td class="tg-vn4c">
+    <select name="cmtsdisabled">
+    <option value="'.$row['cmtsdisabled'].'"/>'.$row['cmtsdisabled'].'</option>
+    <option value="hidden">Hidden</option>
+    <option value="show">Show</option>
+    </select>    
+    </td>
+    <td class="tg-vn4c">Location:</td>
+    <td class="tg-vn4c">
+    <select name="shloc">
+    <option value="'.$row['shloc'].'"/>'.$row['shloc'].'</option>
+    <option value="hidden">Hidden</option>
+    <option value="show">Show</option>
+    </select>    
+    </td>
+  </tr>
+  <tr>
+  <td class="tg-Oord">Email:</td>
+  <td class="tg-Oord">
+  <select name="sheml">
+  <option value="'.$row['sheml'].'"/>'.$row['sheml'].'</option>
+  <option value="hidden">Hidden</option>
+  <option value="show">Show</option>
+  </select>
+  </td>
+  <td class="tg-Oord">Name:</td>
+  <td class="tg-Oord">
+  <select name="shname">
+  <option value="'.$row['shname'].'"/>'.$row['shname'].'</option>
+  <option value="hidden">Hidden</option>
+  <option value="show">Show</option>
+  </select>
+  </td>
+  </tr>
+  <tr>
+  <td class="tg-vn4c">Hobbies:</td>
+  <td class="tg-vn4c">
+  <select name="shhobs">
+  <option value="'.$row['shhobs'].'"/>'.$row['shhobs'].'</option>
+  <option value="hidden">Hidden</option>
+  <option value="show">Show</option>
+  </select>
+  </td>
+  <td class="tg-vn4c">About Me:</td>
+  <td class="tg-vn4c">
+  <select name="shabout">
+  <option value="'.$row['shabout'].'"/>'.$row['shabout'].'</option>
+  <option value="hidden">Hidden</option>
+  <option value="show">Show</option>
+  </select>
+  </td>
+  <tr>';
 	if ($row['oauth_provider'] == "" || empty($row['oauth_provider'])) {
-		echo 'Password:(leave blank if no change)<br />
-		<input type="text" name="password" /><p>';
+		echo '
+		<td class="tg-Oord">Password:(leave blank if no change)</td>
+    <td class="tg-Oord"><input type="text" name="password" /></td>
+    <td class="tg-Oord"></td>
+    <td class="tg-Oord"></td>';
 	}
-	echo'<input type="submit" name="settings" value="Update" /><br />Click on link below if you wish to delete your account.
-  <br /><a href="'.$setting['siteurl'].'index.php?act=profile&delete='.$row['id'].'" title="Are you sure you want to delete your account?" style="text-decoration:none;color:#000;">Delete Account?</a>
-	</form>
-	</div></div>';
+  echo '
+  </tr>
+  <tr>
+    <th class="tg-s6z2" colspan="4">Deactivate Account:</th>
+  </tr>
+  <tr>
+    <td class="tg-vn4c" colspan="2">Click on link if you wish to deactivate your account.</td>
+    <td class="tg-vn4c" colspan="2">
+    <select name="deact">
+    <option value="'.$row['deact'].'"/>'.$row['deact'].'</option>
+    <option value="deactivate">Deactivate</option>
+    <option value="activate">Activate</option>
+    </select>    
+  </tr>
+  <tr>
+    <th class="tg-s6z2" colspan="4">Update Account:</th>
+  </tr>
+  <tr>
+    <td class="tg-vn4c" colspan="2">Update:</td>
+    <td class="tg-vn4c" colspan="2"><input type="submit" name="settings" value="Update" /></td>
+  </tr>        
+  </table>
+  </form> 
+  </div></div>';
 	$query->close();
 }
 ?>
